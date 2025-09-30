eedac9945b21ea8e3e6405fde129585afe013977
Juan Carlos Baez <juancarlos.baez@bc3.net>
2025-09-30 15:46:45 +0200

    Addition of a new file that contains the packages needed, so you do not have to load all the packages every time

---
+++ R/package_imports.R
@@ -0,0 +1,15 @@
+# R/package_imports.R
+#' @keywords internal
+#'
+#' This file imports the necessary functions from external packages
+#' so you can use them without :: in your package functions.
+
+# --- dplyr functions ---
+#' @import dplyr
+# --- magrittr pipe ---
+#' @importFrom magrittr %>%
+# --- assertthat for checks ---
+#' @importFrom assertthat assert_that
+# --- tibble functions ---
+#' @importFrom tibble deframe
+NULL
